f39a74a3bd6d2749bbbca7f1743369a00e8a1774
Nitesh961997 <niteshkavathankar@gmail.com>
2020-05-01 14:36:48 +0530

    case statement added

--- Assignment/employeeSalary.sh
+++ Assignment/employeeSalary.sh
@@ -1,9 +1,26 @@
 #!/bin/bash -x
 echo  Welcome to Employee Wage Computation Program
-present=1
+fullTimr=2
+partTime=1
 absent=0
-observation=$((RANDOM%2))
+observation=$((RANDOM%3))
 perHourSalary=20
 fullTimeHours=8
 partTimeHours=4
+case $observation in
+	0)
+		echo Employee is Absent
+		;;
+	1)
+		echo Employee is working partime
+		;;
+	2)
+		echo Employee is working fulltime
+		;;
+	*)
+		echo invalid input
+		;;
+esac
+	
+
 
